ef848e4dd372add4f9cd0b8cb517cd97efa642d5
Michael McGee <michael@pantaloons.co.nz>
2011-08-14 18:17:19 +1200

    additional functions

--- camera.h
+++ camera.h
@@ -2,6 +2,14 @@
 #include <opencv/highgui.h>
 #include <libfreenect_sync.h>
 
-void updateCamera();
-int*** readCamera();
-int*** readSilhouette();
+int initCamera();
+
+void swapRgbBuffers();
+void swapDepthBuffers();
+
+void *cameraLoop(void *arg);
+void rgbFunc(freenect_device *dev, void *rgb, uint32_t timestamp);
+void depthFunc(freenect_device *dev, void *v_depth, uint32_t timestamp);
+
+int *readCamera();
+int *readSilhouette();
