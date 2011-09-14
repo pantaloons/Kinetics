788ad2760982586b85761f5e4e3e41fef9f5ac3a
Michael McGee <michael@pantaloons.co.nz>
2011-09-14 15:39:10 +1200

    empty marker template

---
+++ marker.c
@@ -0,0 +1,14 @@
+/**
+ * Locate the blue marker in the image
+ */
+int findMarker(int* rgb, int* outx, int* outy) {
+	
+	/* Locate the marker in the rgb image (640x480),
+	 * return 0 if it is not found, if it is found
+	 * return 1 and save the x and y position (center)
+	 * to outx, outy */
+	
+	*outx = -1;
+	*outy = -1;
+	return 0;
+}
---
+++ marker.h
@@ -0,0 +1,4 @@
+/**
+ * Locate the blue marker in the image
+ */
+int findMarker(int* rgb, int* outx, int* outy);
