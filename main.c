aa722e7ebbdac8abcaf5dc43c3dfe25125b0cfb5
daniel <danny@danny-Vostro-3550.(none)>
2011-09-29 17:42:52 +1300

    Removed some problems in marker.c

--- main.c
+++ main.c
@@ -48,6 +48,7 @@ void renderFour();
 void (*paintFunc)(void) = &renderOne;
 
 int main(int argc, char** argv) {
+	markerInit();
 	if(initCamera()) {
 		printf("camera initialization failed!\n");
 		return 1;
@@ -73,6 +74,7 @@ int main(int argc, char** argv) {
 	
 	initialize();
 	controlInit();
+
 	
 	result = pthread_create(&runThread, NULL, runLoop, NULL);
 	if(result) {
--- marker.c
+++ marker.c
@@ -15,22 +15,17 @@ int list[640 * 480];
 int qindex = 0;
 int lindex = 0;
 
-int rgbToHue2(int ri, int gi, int bi) {
-	double r = ri / 255.0f;
-	double g = gi / 255.0f;
-	double b = bi / 255.0f;
-	double max = MAX(r, MAX(g, b));
-	double min = MIN(r, MIN(g, b));
-	double d = max - min;
-	double h = 0;
-	if(d < 1e-8) return h;
-	else {
-		if(max - r < 1e-8) h = (g - b) / d + (g < b ? 6 : 0);
-		else if(max - g < 1e-8) h = (b - r) / d + 2;
-		else if(max - b < 1e-8) h = (r - g) / d + 4;
-	}
-	h /= 6.0f;
-	return (int)(h * 255);
+CvMoments* moments;
+IplImage* img2;
+IplImage* imgThreshed;
+IplImage* imgHSV;
+
+void markerInit(void)
+{
+	moments = (CvMoments*)malloc(sizeof(CvMoments));
+	img2 = cvCreateImageHeader(cvSize(640,480), 8, 3);
+	imgThreshed = cvCreateImage(cvGetSize(img2), 8, 1);
+	imgHSV = cvCreateImage(cvGetSize(img2), 8, 3);
 }
 
 void floodFill(uint8_t *image) {
@@ -66,25 +61,22 @@ void floodFill(uint8_t *image) {
 	}
 }
 
-IplImage* GetThresholdedImage(IplImage* img, int hue)
+void GetThresholdedImage(IplImage* img, int hue)
 {
 	// Convert the image into an HSV image
-	IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
+	
 	cvCvtColor(img, imgHSV, CV_RGB2HSV);
-
-
-	IplImage* imgThreshed = cvCreateImage(cvGetSize(img), 8, 1);
-
-
+	
 	// Values 20,100,100 to 30,255,255 working perfect for yellow at around 6pm
-	cvInRangeS(imgHSV, cvScalar(hue-3, 100, 100,1), cvScalar(hue+3, 200, 200,1), imgThreshed);
+	cvInRangeS(imgHSV, cvScalar(hue-5, 100, 100,1), cvScalar(hue+5, 200, 200,1), imgThreshed);
 	
 	
-	IplConvKernel* k = cvCreateStructuringElementEx(5,5,1,1,CV_SHAPE_ELLIPSE,NULL);
+	
+	IplConvKernel* k = cvCreateStructuringElementEx(3,3,1,1,CV_SHAPE_ELLIPSE,NULL);
 	cvErode(imgThreshed, imgThreshed, k, 1);
     cvReleaseStructuringElement(&k);
     
-    floodFill(imgThreshed->imageData);
+    //floodFill(imgThreshed->imageData);
 	
 	pthread_mutex_lock(&hsvMutex);
 		
@@ -106,8 +98,6 @@ IplImage* GetThresholdedImage(IplImage* img, int hue)
 	pthread_mutex_unlock(&hsvMutex);
 
 	cvReleaseImage(&imgHSV);
-
-	return imgThreshed;
 }
 
 
@@ -122,20 +112,15 @@ int findMarker(int hue, uint8_t* rgb, int* outx, int* outy) {
 	 * return 0 if it is not found, if it is found
 	 * return 1 and save the x and y position (center)
 	 * to outx, outy */
-	
-	//printf("Hue %d\n", rgbToHue2(rgb[0], rgb[1], rgb[2]));
-
-	
-	IplImage* img2=cvCreateImageHeader(cvSize(640,480), 8, 3);
 	cvSetData(img2, rgb, 640*3);
 	
 	// Holds the yellow thresholded image (yellow = white, rest = black)
-	IplImage* imgThresh = GetThresholdedImage(img2,hue);
+	GetThresholdedImage(img2,hue);
+	
 		
 	
 	// Calculate the mouments to estimate the position of the ball
-	CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));
-	cvMoments(imgThresh, moments, 1);
+	cvMoments(imgThreshed, moments, 1);
 
 	// The actual moment values
 	double moment10 = cvGetSpatialMoment(moments, 1, 0);
@@ -145,11 +130,8 @@ int findMarker(int hue, uint8_t* rgb, int* outx, int* outy) {
 	// Holding the last and current ball positions
 	*outx = moment10/area;
 	*outy = moment01/area;
-	
-	//cvReleaseImage(&img2);
-	free(moments);
-	
-	if(*outx < 1 || *outy < 1){
+		
+	if(area < 50){//*outx < 1 || *outy < 1){
 		
 		return 0;
 		
